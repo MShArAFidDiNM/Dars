181a86980af9bc5d1e5db038c8ac65ee6a5d8d03
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-03-31 11:58:27 +0500

    Array prime number

---
+++ Dars/Dars/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number;
+            Console.WriteLine("Enter number : ");
+            
+            Number = int.Parse(Console.ReadLine());
+            Number *= 2;
+            int[] array = new int[Number];            
+            
+            for (int i = 1; i < array.Length; i+=2)
+            {
+                Console.Write($"{ i}, ");
+            }
+            
+            Main(args);
+        }
+    }
+}
